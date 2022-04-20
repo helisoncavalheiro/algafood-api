cab39d89a332361c42b78b0ec129ebea9e01cf54
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2022-04-19 21:18:53 -0300

    exemplos consultas JPA e spring data jpa

--- src/main/java/com/helison/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/helison/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,8 @@
 package com.helison.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.helison.algafood.domain.model.Cozinha;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+  List<Cozinha> findTodasByNomeContaining(String nome);
+
+  Optional<Cozinha> findByNome(String nome);
 }
--- src/main/java/com/helison/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/helison/algafood/domain/repository/RestauranteRepository.java
@@ -1,11 +1,18 @@
 package com.helison.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import com.helison.algafood.domain.model.Restaurante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+  List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+  List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 }
