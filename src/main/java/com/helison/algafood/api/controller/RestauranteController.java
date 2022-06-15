1f65b9d426133b337c4d697cfb8987ee7b72a906
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2022-06-14 22:47:13 -0300

    ignora restaurantes no retorno da API

--- src/main/java/com/helison/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/helison/algafood/api/controller/RestauranteController.java
@@ -74,7 +74,7 @@ public class RestauranteController {
     }
 
     try {      
-      BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id");
+      BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento");
   
       Restaurante restauranteSalvo = cadastroRestaurante.salvar(restauranteAtual.get());
 
