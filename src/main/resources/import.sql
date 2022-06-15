9fd10d3df929ecfa763750e76a98190a197e7787
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2022-06-14 23:07:54 -0300

    adiciona endereço em um restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,6 @@
 insert into cozinha(nome) values ('Tailandesa');
 insert into cozinha(nome) values ('Indiana');
 
-insert into restaurante(nome, taxa_frete, cozinha_id) values("Thai Gourmet", 9.5, 2);
-insert into restaurante(nome, taxa_frete, cozinha_id) values("Thai Delivery", 10, 2);
-insert into restaurante(nome, taxa_frete, cozinha_id) values("Tuk Tuk Delivery", 10, 1);
-
 insert into estado(nome) values ("Paraná");
 insert into estado(nome) values ("Santa Catarina");
 insert into estado(nome) values ("Rio Grande do Sul");
@@ -24,5 +20,9 @@ insert into forma_pagamento(descricao) values ("PIX");
 insert into permissao(nome, descricao) values ("cliente", "Cliente do restaurante");
 insert into permissao(nome, descricao) values ("atendente", "Atendente do restaurante");
 insert into permissao(nome, descricao) values ("dono", "Dono do restaurante");
+
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
