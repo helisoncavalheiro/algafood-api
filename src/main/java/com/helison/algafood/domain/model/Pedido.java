7de4fc95af07c0f937e12441b16dc289451c8dd2
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2022-11-22 22:53:53 -0300

    cria pedido e item pedido

---
+++ src/main/java/com/helison/algafood/domain/model/Pedido.java
@@ -0,0 +1,77 @@
+package com.helison.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
+
+    @Column(nullable = false)
+    private BigDecimal taxaFrete;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @Column(nullable = true)
+    private LocalDateTime dataConfirmacao;
+
+    @Column(nullable = true)
+    private LocalDateTime dataCancelamento;
+
+    @Column(nullable = true)
+    private LocalDateTime dataEntrega;
+
+    @Embedded
+    @JsonIgnore
+    private Endereco enderecoEntrega;
+
+    @ManyToOne
+    @JoinColumn(name = "forma_pagamento_id", nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", nullable = false)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id", nullable = false)
+    private Usuario cliente;
+
+    @Column(nullable = false)
+    private StatusPedido statusPedido;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+}
