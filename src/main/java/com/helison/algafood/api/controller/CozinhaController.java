9596a686ce86d1ae7adabe1e1c29cbd2a08451e4
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2022-11-24 22:26:36 -0300

    simplifica lançamento de exceções
    usa a annotation ResponseStatus na exception para gerar uma resposta padrão quando esta for lançada

--- src/main/java/com/helison/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/helison/algafood/api/controller/CozinhaController.java
@@ -72,18 +72,8 @@ public class CozinhaController {
   }
 
   @DeleteMapping("/{cozinhaId}")
-  public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
-    try {
-      cadastroCozinha.excluir(cozinhaId);
-      return ResponseEntity.noContent().build();
-
-    } catch (EntidadeEmUsoException e) {
-      return ResponseEntity.status(HttpStatus.CONFLICT).build();
-
-    } catch (EntidadeNaoEncontradaException e) {
-      return ResponseEntity.notFound().build();
-
-    }
-
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void remover(@PathVariable Long cozinhaId) {
+    cadastroCozinha.excluir(cozinhaId);
   }
 }
